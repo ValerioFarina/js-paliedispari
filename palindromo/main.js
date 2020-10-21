aa097a1411b118837f4a97fa0e586d7fb3ead0aa
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-21 18:20:14 +0200

    Semplificata la funzione "isPalindrome"

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,33 +1,42 @@
-/* CONSEGNA: scrivere una funzione per valutare se una parola è palindroma oppure no
-             (una parola è palindroma se la posso leggere nello stesso modo dall'inizio alla fine e viceversa, ad esempio "anna" è palindroma, "andrea" no) */
+/*  CONSEGNA: scrivere una funzione per valutare se una parola è palindroma oppure no
+             (una parola è palindroma se la posso leggere nello stesso modo dall'inizio alla fine e viceversa, ad esempio "anna" è palindroma, "andrea" no)  */
 
 
-/* Dichiaro una funzione isPalindrome per valutare se una parola è palindroma oppure no.
-   Questa funzione ha un solo parametro formale.
-   Per utilizzarla correttamente, occorre passargli come parametro effettivo una stringa */
+/*  Dichiaro una funzione isPalindrome per valutare se una parola è palindroma oppure no.
+    Questa funzione ha un solo parametro formale.
+    Per utilizzarla correttamente, occorre passarle come parametro effettivo una stringa  */
 function isPalindrome(word) {
-    // "Divido" la stringa word in una parte sx (leftHalf) e una parte dx (rightHalf)
-    var leftHalf = word.slice(0, Math.floor(word.length / 2));
-    var rightHalf = word.slice(Math.ceil(word.length / 2), word.length);
-    // Creo un ciclo for che effettua un numero di iterazioni pari alla lunghezza della parte sx (che è sempre uguale alla lunghezza della parte dx)
-    for (var i=0; i<leftHalf.length; i++) {
-        /* Scorro contemporaneamente la parte sx e quella dx:
-            - la parte sx la scorro da sx a dx (la posizione corrente è in questo caso data da leftPosition)
-            - la parte dx la scorro da dx a sx (la posizione corrente è in questo caso data da rightPosition) */
-        leftPosition = i;
-        rightPosition = word.length - 1 - i;
-        // Confronto carattere per carattere la parte sx e quella dx (si ricordi però che le due parti vanno lette in sensi opposti)
-        if (word[leftPosition] != word[rightPosition]) {
-            // Non appena la parte sx e quella dx (lette in due sensi opposti) differiscono di un carattere, ritorno false ed esco dalla funzione
+    /*  Salvo nella variabile middle un valore che rappresenta idealmente il centro della stringa word:
+        - se word.length è dispari, middle è esattamente il centro della stringa;
+        - se word.length è pari, la stringa può essere divisa in due parti di uguale lunghezza, una di sx e una di dx,
+          e in tal caso middle indicherà il punto in cui inizia la parte di dx  */
+    var middle = Math.floor(word.length / 2);
+    for (var i=0; i<middle; i++) {
+    /*  - Confronto il primo carattere di word (cioè, word[0]) con l'ultimo (cioè, word[word.length - 1 - 0]).
+          Se sono diversi, ritorno falso e esco dalla funzione.
+        - Confronto il secondo carattere di word (cioè, word[1]) con il penultimo (cioè, word[word.length - 1 - 1]).
+          Se sono diversi, ritorno falso e esco dalla funzione.
+        - Confronto il terzo carattere di word (cioè, word[2]) con il terzultimo (cioè, word[word.length - 1 - 2]).
+          Se sono diversi, ritorno falso e esco dalla funzione.
+          etc......  */
+        if (word[i] != word[word.length - 1 - i]) {
             return false;
         }
     }
-    /* Se il ciclo for esegue tutte le possibili iterazioni senza mai entrare nell'istruzione condizionale, allora significa che la parte dx
-       non è altro che la parte sx letta al contrario, e quindi la stringa word è palindroma */
+    // Se il ciclo for esegue tutte le possibili iterazioni senza mai eseguire l'istruzione condizionale, allora significa che la parola è palindroma
     return true;
 }
 
-console.log('\'anna\' è palindromo? ' + isPalindrome('anna'));
-console.log('\'ingegni\' è palindromo? ' + isPalindrome('ingegni'));
-console.log('\'andrea\' è palindromo? ' + isPalindrome('andrea'));
-console.log('\'mamma\' è palindromo? ' + isPalindrome('mamma'));
+
+// ********** TESTS **********
+var word = 'anna';
+console.log('Is ' + '\'' + word + '\'' + ' a palindrome? ' + isPalindrome(word));
+
+word = 'ingegni';
+console.log('Is ' + '\'' + word + '\'' + ' a palindrome? ' + isPalindrome(word));
+
+word = 'andrea';
+console.log('Is ' + '\'' + word + '\'' + ' a palindrome? ' + isPalindrome(word));
+
+word = 'mamma';
+console.log('Is ' + '\'' + word + '\'' + ' a palindrome? ' + isPalindrome(word));
--- pariedispari/main.js
+++ pariedispari/main.js
@@ -1,9 +1,9 @@
-/* CONSEGNA: scrivere una (o più di una) funzione per simulare il gioco di pari e dispari tra l'utente e il computer.
+/*  CONSEGNA: scrivere una (o più di una) funzione per simulare il gioco di pari e dispari tra l'utente e il computer.
              L'utente sceglie un numero tra 1 e 5 e sceglie "pari" o "dispari".
              Il computer genera un secondo numero casuale tra 1 e 5.
              Per vedere chi dei due vince, si deve fare la somma dei numeri dell'utente e del computer e stabilire se il risultato è pari o dispari.
              Ad esempio, l'utente inserisce il numero 1 e indica come scelta "pari"; il computer genera casualmente il numero 3.
-             In questo caso ha vinto l'utente (perché 1+3 = 4, che è pari) */
+             In questo caso ha vinto l'utente (perché 1+3 = 4, che è pari)  */
 
 
 // Fisso gli estremi del range entro il quale dovrebbero essere compresi i numeri dell'utente e del computer
@@ -13,17 +13,15 @@ var sup = 5;
 do {
     // Chiedo all'utente di inserire un numero da 1 a 5
     var userNumber = parseInt(prompt('Inserisci un numero da ' + inf + ' a ' + sup));
-/*
-    Continuo a chiederglielo fintanto che l'input inserito
-    o non è un numero,
-    oppure è un numero, ma non è compreso tra 1 e 5
-*/
+    /*  Continuo a chiederglielo fintanto che l'input inserito
+        o non è un numero,
+        oppure è un numero, ma non è compreso tra 1 e 5  */
 } while (isNaN(userNumber) || userNumber < inf || userNumber > sup);
 
 do {
     // Chiedo all'utente di inserire "pari" o "dispari" (l'input viene direttamente trasformato in lower-case)
     var userChoice = prompt('Scegli tra pari e dispari').toLowerCase();
-//  Continuo a chiederglielo fintanto che l'input inserito non è nè "pari" nè "dispari"
+    //  Continuo a chiederglielo fintanto che l'input inserito non è nè "pari" nè "dispari"
 } while (userChoice != 'pari' && userChoice != 'dispari');
 
 // Simulo il gioco di pari e dispari tra l'utente e il computer invocando la funzione evenOddGame e passandole come argomenti i due input inseriti dall'utente
@@ -33,10 +31,12 @@ evenOddGame(inf, sup, userNumber, userChoice);
 
 // ********** LE MIE FUNZIONI **********
 
+
 // Definisco una funzione che genera un numero intero random compreso tra min e max
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
 
 // Definisco una funzione per stabilire se un numero è pari oppure dispari
 function isEven(number) {
@@ -47,18 +47,18 @@ function isEven(number) {
     }
 }
 
+
 // Definisco una funzione che prende in input due numeri e restituisce la loro somma
 function sum(num1, num2) {
     return num1 + num2;
 }
 
-/*
-    Definisco una funzione per simulare il gioco di pari e dispari tra utente e computer.
+
+/*  Definisco una funzione per simulare il gioco di pari e dispari tra utente e computer.
     Questa funzione ha quattro parametri:
         - inf e sup, che indicano gli estremi (inferiore e superiore, rispettivamente) del range dal quale il computer deve "segliere" il numero;
         - number, che indica il numero inserito dall'utente (anche questo dovrebbe essere compreso tra inf e sup);
-        - evenOrOdd, che dovrebbe essere o la stringa "pari" o la stringa "dispari" inserita dall'utente.
-*/
+        - evenOrOdd, che dovrebbe essere o la stringa "pari" o la stringa "dispari" inserita dall'utente.  */
 function evenOddGame(inf, sup, number, evenOrOdd) {
     // mostro in console le scelte dell'utente
     console.log('Utente: ' + number + ', ' + evenOrOdd);
